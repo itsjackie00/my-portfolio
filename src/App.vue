6a95b1986c595f74803dbc450de5e0b6797cbba7
itsjackie00 <jackiedanso33@gmail.com>
2024-06-20 18:41:30 +0200

    started project

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 
 <template>
-  <h1>Ciao</h1>
-  <i class="fa-solid fa-house"></i>
+ <header>
+  <NavigationComponent/>
+ </header>
 </template>
 
 <script>
+  import NavigationComponent from './components/NavigationComponent.vue'
   export default {
     name: 'App',
+    components: {
+      NavigationComponent
+    }
   }
 </script>
 
 <style lang="scss" scoped>
-
+header{
+height: 100vh;
+width: 100vw;
+background-color: #211036;
+}
+.debug{
+  border: 2px solid red;
+}
 </style>
---
+++ src/components/NavigationComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav class="container text-white p-5">
+        navigation
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: 'NavigationComponent',
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
